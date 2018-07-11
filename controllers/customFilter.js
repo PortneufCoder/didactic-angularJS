2703ab65cc13d7a95d4396304996c0544cb5b4a7
Victor Ajani <ajavi1@qc.cgi.ca>
2018-07-11 12:08:10 -0400

    Added some css and images. Tested angular filtering

--- controllers/customFilter.js
+++ controllers/customFilter.js
@@ -30,5 +30,22 @@ customFilter.filter('addBold', function() {
 customFilter.controller('date', function($scope) {
 
     $scope.theDate = Date.now();
+
+})
+
+customFilter.controller('grocery', function($scope) {
+
+    $scope.foodList = [
+        {name: `Apples`, location: './images/apples.jpg', group: 'Fruits'},
+        {name: `Potato`, location: './images/potato.jpg', group: 'Vegetables'},
+        {name: `Beans`, location: './images/beans.jpg', group: 'Vegetables'},
+        {name: `Lentils`, location: './images/lentil.jpg', group: 'Grain'},
+        {name: `Bread`, location: './images/bread.jpg', group: 'Grain'},
+        {name: `Cheese`, location: './images/cheese.jpg', group: 'Dairy'},
+        {name: `Rhubarb`, location: './images/rhubarb.jpg', group: 'Fruit'},
+        {name: `Butter`, location: './images/butter.jpg', group: 'Dairy'}
+    ]
+
+    $scope.selectedFoodGroup = "";
     
 })
